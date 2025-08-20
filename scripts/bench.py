8d8e727a27f7bb32903500a27eb8efb3178a1d6c
Peter Gessler <gesslerpd@users.noreply.github.com>
2025-08-19 19:35:32 -0500

    improve performance

--- scripts/bench.py
+++ scripts/bench.py
@@ -3,19 +3,19 @@ import os
 # set before importing `msgpack`
 os.environ["MSGPACK_PUREPYTHON"] = "1"
 
-from msgpack_stream import unpack, unpack_stream
-from mmap import mmap, ACCESS_READ
+from msgpack_stream import unpack, unpack_stream, pack, pack_stream
+from mmap import mmap, ACCESS_READ, ACCESS_WRITE
 import argparse
 import timeit
 
-from msgpack import unpackb
+from msgpack import unpackb, packb
 
 
 FILE = "scripts/obj.msgpack"
 
 
 def main(mapped):
-    with open(FILE, "rb", buffering=0) as fd:
+    with open(FILE, "rb") as fd:
         if mapped:
             with mmap(fd.fileno(), 0, access=ACCESS_READ) as mm:
                 return unpack(mm.read())
@@ -24,7 +24,7 @@ def main(mapped):
 
 
 def stream(mapped):
-    with open(FILE, "rb", buffering=0) as fd:
+    with open(FILE, "rb") as fd:
         if mapped:
             with mmap(fd.fileno(), 0, access=ACCESS_READ) as mm:
                 return unpack_stream(mm)
@@ -33,7 +33,7 @@ def stream(mapped):
 
 
 def other(mapped):
-    with open(FILE, "rb", buffering=0) as fd:
+    with open(FILE, "rb") as fd:
         if mapped:
             with mmap(fd.fileno(), 0, access=ACCESS_READ) as mm:
                 return unpackb(mm.read(), strict_map_key=False)
@@ -41,21 +41,80 @@ def other(mapped):
             return unpackb(fd.read(), strict_map_key=False)
 
 
+def serialize_main(obj, mapped):
+    with open("temp", "wb") as fd:
+        if mapped:
+            with mmap(-1, 3955122, access=ACCESS_WRITE) as mm:
+                mm.write(pack(obj))
+        else:
+            fd.write(pack(obj))
+
+
+def serialize_stream(obj, mapped):
+    with open("temp", "wb") as fd:
+        if mapped:
+            with mmap(-1, 3955122, access=ACCESS_WRITE) as mm:
+                pack_stream(mm, obj)
+        else:
+            pack_stream(fd, obj)
+
+
+def serialize_other(obj, mapped):
+    with open("temp", "wb") as fd:
+        if mapped:
+            with mmap(-1, 3955122, access=ACCESS_WRITE) as mm:
+                mm.write(packb(obj))
+        else:
+            fd.write(packb(obj))
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", type=int, default=25, help="Number of runs")
+    parser.add_argument(
+        "-m", "--mapped", action="store_true", help="Use memory mapping"
+    )
+    args = parser.parse_args()
+
     _globals = {
         "main": main,
         "stream": stream,
         "other": other,
+        "mapped": args.mapped,
     }
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--number", type=int, default=25, help="Number of runs")
-    args = parser.parse_args()
 
-    t_main = timeit.timeit("main(True)", number=args.number, globals=_globals)
-    # this needs to be mmap for good performance
-    t_stream = timeit.timeit("stream(True)", number=args.number, globals=_globals)
-    t_other = timeit.timeit("other(True)", number=args.number, globals=_globals)
+    _serialize = {
+        "main": serialize_main,
+        "stream": serialize_stream,
+        "other": serialize_other,
+        "obj": stream(False),
+        "mapped": args.mapped,
+    }
+
+    t_main = timeit.timeit("main(mapped)", number=args.number, globals=_globals)
+    t_stream = timeit.timeit("stream(mapped)", number=args.number, globals=_globals)
+    t_other = timeit.timeit("other(mapped)", number=args.number, globals=_globals)
 
     print(f"main: {t_main:.6f}s total, {t_main / args.number:.6f}s per call")
     print(f"stream: {t_stream:.6f}s total, {t_stream / args.number:.6f}s per call")
     print(f"other: {t_other:.6f}s total, {t_other / args.number:.6f}s per call")
+
+    t_main_s = timeit.timeit(
+        "main(obj, mapped)", number=args.number, globals=_serialize
+    )
+    t_stream_s = timeit.timeit(
+        "stream(obj, mapped)", number=args.number, globals=_serialize
+    )
+    t_other_s = timeit.timeit(
+        "other(obj, mapped)", number=args.number, globals=_serialize
+    )
+
+    print(
+        f"main serialize: {t_main_s:.6f}s total, {t_main_s / args.number:.6f}s per call"
+    )
+    print(
+        f"stream serialize: {t_stream_s:.6f}s total, {t_stream_s / args.number:.6f}s per call"
+    )
+    print(
+        f"other serialize: {t_other_s:.6f}s total, {t_other_s / args.number:.6f}s per call"
+    )
--- src/msgpack_stream/_msgpack.py
+++ src/msgpack_stream/_msgpack.py
@@ -34,202 +34,192 @@ s64_b_unpack = s64_b_t.unpack
 f32_b_unpack = f32_b_t.unpack
 f64_b_unpack = f64_b_t.unpack
 
+_B = tuple([bytes([i]) for i in range(256)])
+
 
 def pack_stream(stream, obj):
-    if isinstance(obj, dict):
+    _ps = pack_stream
+    _b = _B
+    _write = stream.write
+    _type = type(obj)
+    if _type is int:
+        i = obj
+        if 0 <= i <= 0x7F:  # positive fixint
+            _write(_b[i])
+        elif -32 <= i < 0:  # negative fixint
+            _write(_b[i & 0xFF])
+        elif i < 0:  # wider negative
+            ai = -i
+            if ai <= 0x80:
+                _write(b"\xd0")
+                s8_b_pack(stream, i)
+            elif ai <= 0x80_00:
+                _write(b"\xd1")
+                s16_b_pack(stream, i)
+            elif ai <= 0x80_00_00_00:
+                _write(b"\xd2")
+                s32_b_pack(stream, i)
+            elif ai <= 0x80_00_00_00_00_00_00_00:
+                _write(b"\xd3")
+                s64_b_pack(stream, i)
+            else:
+                raise ValueError("int too large")
+        elif i <= 0xFF:
+            _write(b"\xcc")
+            u8_b_pack(stream, i)
+        elif i <= 0xFF_FF:
+            _write(b"\xcd")
+            u16_b_pack(stream, i)
+        elif i <= 0xFF_FF_FF_FF:
+            _write(b"\xce")
+            u32_b_pack(stream, i)
+        elif i <= 0xFF_FF_FF_FF_FF_FF_FF_FF:
+            _write(b"\xcf")
+            u64_b_pack(stream, i)
+        else:
+            raise ValueError("uint too large")
+    elif _type is float:
+        _write(b"\xcb")
+        f64_b_pack(stream, obj)
+    elif _type is dict:
         map_length = len(obj)
         if map_length <= 0x0F:
-            u8_b_pack(stream, 0b10000000 + map_length)
+            _write(_b[0x80 | map_length])
         elif map_length <= 0xFF_FF:
-            stream.write(b"\xde")
+            _write(b"\xde")
             u16_b_pack(stream, map_length)
         elif map_length <= 0xFF_FF_FF_FF:
-            stream.write(b"\xdf")
+            _write(b"\xdf")
             u32_b_pack(stream, map_length)
         else:
             raise ValueError("map too large", obj)
-        for key, value in obj.items():
-            pack_stream(stream, key)
-            pack_stream(stream, value)
-    elif isinstance(obj, bool):
-        stream.write(b"\xc3" if obj else b"\xc2")
-    elif isinstance(obj, int):
-        int_negative = obj < 0
-        int_abs = abs(obj)
-        if int_negative:
-            if int_abs <= 0b100000:
-                s8_b_pack(stream, obj)
-            elif int_abs <= 0x80:
-                stream.write(b"\xd0")
-                s8_b_pack(stream, obj)
-            elif int_abs <= 0x80_00:
-                stream.write(b"\xd1")
-                s16_b_pack(stream, obj)
-            elif int_abs <= 0x80_00_00_00:
-                stream.write(b"\xd2")
-                s32_b_pack(stream, obj)
-            elif int_abs <= 0x80_00_00_00_00_00_00_00:
-                stream.write(b"\xd3")
-                s64_b_pack(stream, obj)
-            else:
-                raise ValueError("int too large")
-        else:
-            if int_abs <= 0b1111111:
-                u8_b_pack(stream, obj)
-            elif int_abs <= 0xFF:
-                stream.write(b"\xcc")
-                u8_b_pack(stream, obj)
-            elif int_abs <= 0xFF_FF:
-                stream.write(b"\xcd")
-                u16_b_pack(stream, obj)
-            elif int_abs <= 0xFF_FF_FF_FF:
-                stream.write(b"\xce")
-                u32_b_pack(stream, obj)
-            elif int_abs <= 0xFF_FF_FF_FF_FF_FF_FF_FF:
-                stream.write(b"\xcf")
-                u64_b_pack(stream, obj)
-            else:
-                raise ValueError("uint too large")
-    elif isinstance(obj, float):
-        stream.write(b"\xcb")
-        f64_b_pack(stream, obj)
-    elif isinstance(obj, (bytes, bytearray)):
-        bin_length = len(obj)
-        if bin_length <= 0xFF:
-            stream.write(b"\xc4")
-            u8_b_pack(stream, bin_length)
-        elif bin_length <= 0xFF_FF:
-            stream.write(b"\xc5")
-            u16_b_pack(stream, bin_length)
-        elif bin_length <= 0xFF_FF_FF_FF:
-            stream.write(b"\xc6")
-            u32_b_pack(stream, bin_length)
-        else:
-            raise ValueError("bin too large", obj)
-        stream.write(obj)
-    elif isinstance(obj, str):
-        str_bytes = obj.encode("utf-8")
-        str_length = len(str_bytes)
-        if str_length <= 0b11111:
-            u8_b_pack(stream, 0b10100000 + str_length)
-        elif str_length <= 0xFF:
-            stream.write(b"\xd9")
-        elif str_length <= 0xFF_FF:
-            stream.write(b"\xda")
-        elif str_length <= 0xFF_FF_FF_FF:
-            stream.write(b"\xdb")
-        else:
-            raise ValueError("str too large", obj)
-        stream.write(str_bytes)
-    elif isinstance(obj, list):
+        for k, v in obj.items():
+            _ps(stream, k)
+            _ps(stream, v)
+    elif _type is list:
         array_length = len(obj)
         if array_length <= 0x0F:
-            u8_b_pack(stream, 0b10010000 + array_length)
+            _write(_b[0x90 | array_length])
         elif array_length <= 0xFF_FF:
-            stream.write(b"\xdc")
+            _write(b"\xdc")
             u16_b_pack(stream, array_length)
         elif array_length <= 0xFF_FF_FF_FF:
-            stream.write(b"\xdd")
+            _write(b"\xdd")
             u32_b_pack(stream, array_length)
         else:
             raise ValueError("array too large", obj)
-        for value in obj:
-            pack_stream(stream, value)
+        for v in obj:
+            _ps(stream, v)
+    elif _type is str:
+        s = obj.encode("utf-8")
+        sl = len(s)
+        if sl <= 0x1F:
+            _write(_b[0xA0 | sl])
+        elif sl <= 0xFF:
+            _write(b"\xd9")
+        elif sl <= 0xFF_FF:
+            _write(b"\xda")
+        elif sl <= 0xFF_FF_FF_FF:
+            _write(b"\xdb")
+        else:
+            raise ValueError("str too large", obj)
+        _write(s)
     elif obj is None:
-        stream.write(b"\xc0")
+        _write(b"\xc0")
+    elif _type is bool:
+        _write(b"\xc3" if obj else b"\xc2")
+    elif _type is bytes:
+        bin_length = len(obj)
+        if bin_length <= 0xFF:
+            _write(b"\xc4")
+            u8_b_pack(stream, bin_length)
+        elif bin_length <= 0xFF_FF:
+            _write(b"\xc5")
+            u16_b_pack(stream, bin_length)
+        elif bin_length <= 0xFF_FF_FF_FF:
+            _write(b"\xc6")
+            u32_b_pack(stream, bin_length)
+        else:
+            raise ValueError("bin too large", obj)
+        _write(obj)
     else:
         raise TypeError("type not supported:", obj, type(obj))
 
 
 def unpack_stream(stream):
-    first_byte = u8_b_unpack(stream)
-    if first_byte <= 0x7F:
-        obj = first_byte
-    elif first_byte <= 0x8F:
-        map_length = first_byte & 0b1111
-        obj = {unpack_stream(stream): unpack_stream(stream) for _ in range(map_length)}
-    elif first_byte <= 0x9F:
-        array_length = first_byte & 0b1111
-        obj = [unpack_stream(stream) for _ in range(array_length)]
-    elif first_byte <= 0xBF:
-        str_length = first_byte & 0b11111
-        obj = stream.read(str_length).decode("utf-8")
-    elif 0xE0 <= first_byte and first_byte <= 0xFF:
-        stream.seek(-1, 1)
-        obj = s8_b_unpack(stream)
-    elif first_byte == 0xC0:
-        obj = None
-    elif first_byte == 0xC2:
-        obj = False
-    elif first_byte == 0xC3:
-        obj = True
-    elif first_byte == 0xC4:
-        bin_length = u8_b_unpack(stream)
-        obj = stream.read(bin_length)
-    elif first_byte == 0xC5:
-        bin_length = u16_b_unpack(stream)
-        obj = stream.read(bin_length)
-    elif first_byte == 0xC6:
-        bin_length = u32_b_unpack(stream)
-        obj = stream.read(bin_length)
-    elif first_byte == 0xC7:
+    b = stream.read(1)
+    first_byte = b[0]
+    if first_byte <= 0x7F:  # positive fixint
+        return first_byte
+    if first_byte >= 0xE0:  # negative fixint
+        return first_byte - 0x100
+    if first_byte <= 0x8F:  # fixmap
+        mlen = first_byte & 0x0F
+        return {unpack_stream(stream): unpack_stream(stream) for _ in range(mlen)}
+    if first_byte <= 0x9F:  # fixarray
+        alen = first_byte & 0x0F
+        return [unpack_stream(stream) for _ in range(alen)]
+    if first_byte <= 0xBF:  # fixstr
+        slen = first_byte & 0x1F
+        return stream.read(slen).decode("utf-8")
+    if first_byte == 0xC0:
+        return None
+    if first_byte == 0xC2:
+        return False
+    if first_byte == 0xC3:
+        return True
+    if first_byte == 0xC4:
+        bl = u8_b_unpack(stream)
+        return stream.read(bl)
+    if first_byte == 0xC5:
+        bl = u16_b_unpack(stream)
+        return stream.read(bl)
+    if first_byte == 0xC6:
+        bl = u32_b_unpack(stream)
+        return stream.read(bl)
+    if 0xC7 <= first_byte <= 0xC9:
         raise NotImplementedError
-    elif first_byte == 0xC8:
+    if first_byte == 0xCA:
+        return f32_b_unpack(stream)
+    if first_byte == 0xCB:
+        return f64_b_unpack(stream)
+    if first_byte == 0xCC:
+        return u8_b_unpack(stream)
+    if first_byte == 0xCD:
+        return u16_b_unpack(stream)
+    if first_byte == 0xCE:
+        return u32_b_unpack(stream)
+    if first_byte == 0xCF:
+        return u64_b_unpack(stream)
+    if first_byte == 0xD0:
+        return s8_b_unpack(stream)
+    if first_byte == 0xD1:
+        return s16_b_unpack(stream)
+    if first_byte == 0xD2:
+        return s32_b_unpack(stream)
+    if first_byte == 0xD3:
+        return s64_b_unpack(stream)
+    if 0xD4 <= first_byte <= 0xD8:
         raise NotImplementedError
-    elif first_byte == 0xC9:
-        raise NotImplementedError
-    elif first_byte == 0xCA:
-        obj = f32_b_unpack(stream)
-    elif first_byte == 0xCB:
-        obj = f64_b_unpack(stream)
-    elif first_byte == 0xCC:
-        obj = u8_b_unpack(stream)
-    elif first_byte == 0xCD:
-        obj = u16_b_unpack(stream)
-    elif first_byte == 0xCE:
-        obj = u32_b_unpack(stream)
-    elif first_byte == 0xCF:
-        obj = u64_b_unpack(stream)
-    elif first_byte == 0xD0:
-        obj = s8_b_unpack(stream)
-    elif first_byte == 0xD1:
-        obj = s16_b_unpack(stream)
-    elif first_byte == 0xD2:
-        obj = s32_b_unpack(stream)
-    elif first_byte == 0xD3:
-        obj = s64_b_unpack(stream)
-    elif first_byte == 0xD4:
-        raise NotImplementedError
-    elif first_byte == 0xD5:
-        raise NotImplementedError
-    elif first_byte == 0xD6:
-        raise NotImplementedError
-    elif first_byte == 0xD7:
-        raise NotImplementedError
-    elif first_byte == 0xD8:
-        raise NotImplementedError
-    elif first_byte == 0xD9:
-        str_length = u8_b_unpack(stream)
-        obj = stream.read(str_length).decode("utf-8")
-    elif first_byte == 0xDA:
-        str_length = u16_b_unpack(stream)
-        obj = stream.read(str_length).decode("utf-8")
-    elif first_byte == 0xDB:
-        str_length = u32_b_unpack(stream)
-        obj = stream.read(str_length).decode("utf-8")
-    elif first_byte == 0xDC:
-        array_length = u16_b_unpack(stream)
-        obj = [unpack_stream(stream) for _ in range(array_length)]
-    elif first_byte == 0xDD:
-        array_length = u32_b_unpack(stream)
-        obj = [unpack_stream(stream) for _ in range(array_length)]
-    elif first_byte == 0xDE:
-        map_length = u16_b_unpack(stream)
-        obj = {unpack_stream(stream): unpack_stream(stream) for _ in range(map_length)}
-    elif first_byte == 0xDF:
-        map_length = u32_b_unpack(stream)
-        obj = {unpack_stream(stream): unpack_stream(stream) for _ in range(map_length)}
-    else:
-        raise ValueError("invalid first byte", first_byte, hex(first_byte))
-
-    return obj
+    if first_byte == 0xD9:
+        sl = u8_b_unpack(stream)
+        return stream.read(sl).decode("utf-8")
+    if first_byte == 0xDA:
+        sl = u16_b_unpack(stream)
+        return stream.read(sl).decode("utf-8")
+    if first_byte == 0xDB:
+        sl = u32_b_unpack(stream)
+        return stream.read(sl).decode("utf-8")
+    if first_byte == 0xDC:
+        al = u16_b_unpack(stream)
+        return [unpack_stream(stream) for _ in range(al)]
+    if first_byte == 0xDD:
+        al = u32_b_unpack(stream)
+        return [unpack_stream(stream) for _ in range(al)]
+    if first_byte == 0xDE:
+        ml = u16_b_unpack(stream)
+        return {unpack_stream(stream): unpack_stream(stream) for _ in range(ml)}
+    if first_byte == 0xDF:
+        ml = u32_b_unpack(stream)
+        return {unpack_stream(stream): unpack_stream(stream) for _ in range(ml)}
+    raise ValueError("invalid first byte", first_byte, hex(first_byte))
